5339fcefca42a04d880eb3af1024f35721cd540e
Yeimss <jamesherrera364@gmail.com>
2025-08-23 13:45:12 -0500

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const CalculatorApp = () => {
+    return (
+      <View>
+        <Text> Calculator app </Text>
+      </View>
+    )
+}
+
+export default CalculatorApp
